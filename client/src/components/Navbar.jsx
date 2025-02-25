62484a105aeb9f88d5e2e95ba51d98bea2427532
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-02-25 21:37:34 +0530

    add Navbar functions

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {assets} from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-    const [user,setUser] = useState(null);
     const navigate = useNavigate();
   return (
     <div className='flex justify-between items-center py-4'>
@@ -13,7 +12,21 @@ const Navbar = () => {
         <div>
             {
                 user ? 
-                <div></div>
+                <div className='flex items-center gap-2 sm:gap-3'>
+                    <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-2 sm::py-8 rounded-full hover:scale-105 transition-all duration-700'>
+                        <img src={assets.credit_star} alt="credits" className='w-5' />
+                        <p className='text-xs sm:text-sm font-medium text-gray-500'>Credits Left: 50</p>
+                    </button>
+                    <p className='text-gray-699 max-sm:hidden pl-4'>Hi! Subhodeep</p>
+                    <div className='relative group cursor-pointer'>
+                        <img className='w-10 drop-shadow' src={assets.profile_icon} alt="" />
+                        <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                            <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                                <li className='py-1 px-2 pr-10 cursor-pointer'>Logout</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 :
                 <div className='flex items-center gap-2 sm:gap-5'>
                     <p onClick={()=>navigate('/buycredit')} className='cursor-pointer'>Pricing</p>
